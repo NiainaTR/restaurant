e50f82fc47b8b8c0ed05ba9cd5cc7c035d6b4f2a
tsantaniaina <tsantaniainaraherison@gmail.com>
2024-06-10 00:29:16 +0300

    redeploy app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { Routes , Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Menu from "./pages/Menu"
-import MenuWindow from "./components/MenuWindow"
-import FoodDetail from "./pages/FoodDetail"
-import NotFound from "./pages/NotFound"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import CartWindow from "./components/CartWindow"
-import Contacts from "./pages/Contacts"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import MenuWindow from "./components/MenuWindow";
+import FoodDetail from "./pages/FoodDetail";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import CartWindow from "./components/CartWindow";
+import Contacts from "./pages/Contacts";
 
 function App() {
-
   return (
-    <>
-        <MenuWindow></MenuWindow>
-        <CartWindow></CartWindow>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/menu" element={<Menu/>}/>
-          <Route path="/food/:id" element={<FoodDetail/>}/>
-          <Route path="/seconnecter" element={<Login/>}/>
-          <Route path="/creercompte" element={<Signup/>}/>
-          <Route path="/contacts" element={<Contacts/>}/>
-          <Route path="/*" element={<NotFound/>}/>
-        </Routes>    
-    </>
-  )
+    <Router basename="/restaurant">
+      <MenuWindow />
+      <CartWindow />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/food/:id" element={<FoodDetail />} />
+        <Route path="/seconnecter" element={<Login />} />
+        <Route path="/creercompte" element={<Signup />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
